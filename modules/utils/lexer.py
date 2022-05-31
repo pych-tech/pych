6055e8192968903ba8dd690e52ac41aab64a2c1b
theEndurance-del <tomarshubhanshu@protonmail.ch>
2022-05-31 19:50:42 +0530

    Fixed few `lexer` issues

--- modules/utils/lexer.py
+++ modules/utils/lexer.py
@@ -1,4 +1,5 @@
 class Lexer:
+
     def __init__(self) -> None:
         self.character_map: dict[str, str] = {
             ';': 'SEMICOLON',
@@ -32,75 +33,105 @@ class Lexer:
             '<': 'ANGULAR-OPEN',
             '"': 'DOUBLE-QUOTE',
             '\'': 'SINGLE-QUOTE',
-            '_': 'UNDERSCORE'
+            '_': 'UNDERSCORE',
+            ' ': 'SPACE'
         }
+
     def lex(self, string: str) -> list[tuple[str, str]]:
-        character_list: list[tuple[str, str]] = []
+        evalutated_token_stream: str = ""
         token_map: list[tuple[str, str]] = []
         is_integer: bool = False
         is_decimal: bool = False
         is_string_token: bool = False
+        
         for token in string:
+        
             if token.isalpha():
-                character_list.append((token, 'CHAR'))
+                evalutated_token_stream+=token
                 is_integer: bool = False
                 is_decimal: bool = False
                 is_string_token: bool = True
+        
             elif token.isdigit():
-                character_list.append((token, 'DIGIT'))
-                if not (is_string_token and is_decimal):
+                evalutated_token_stream+=token
+        
+                if not (is_string_token or is_decimal):
                     is_integer = True
                 else:
                     is_integer = False
+        
             else:
-                token_property = self.character_map.get(token)
+                token_property = self.character_map.get(token, 'UNKNOWN')
                 if is_string_token:
-                    token_stream = ""
-                    for token_item, _ in character_list:
-                        token_stream+=token_item
-                    token_map.append((token_stream, 'STRING'))
-                    token_map.append((token, self.character_map.get(token)))
-                    is_string_token = False
-                    character_list.clear()
-                elif is_integer:
-                    if token_property == 'STOP':
-                        character_list.append((token, token_property))
-                        is_decimal = True
-                        is_integer = False
+                    if (token_property == "MINUS"
+                        or token_property == "UNDERSCORE"):
+                        evalutated_token_stream+=token
                     else:
                         token_stream = ""
-                        for token_item, _ in character_list:
+        
+                        for token_item in evalutated_token_stream:
                             token_stream+=token_item
+                        
+                        token_map.append((token_stream, 'STRING'))
+                        token_map.append((token, token_property))
+
+                        is_string_token = False
+                        evalutated_token_stream = ""
+                
+                elif is_integer:
+                    if token_property == 'STOP':
+                        evalutated_token_stream+=token
+                        is_decimal = True
+                        is_integer = False
+                
+                    else:
+                        token_stream = ""
+                        for token_item in evalutated_token_stream:
+                            token_stream+=token_item
+                        
                         token_map.append((token_stream, 'NUMBER'))
                         token_map.append((token, token_property))
-                        character_list.clear()
+                        
+                        evalutated_token_stream = ""
                         is_decimal = False
                         is_integer = False
+                
                 elif is_decimal:
                     token_stream = ""
-                    for token_item, _ in character_list:
+                    for token_item in evalutated_token_stream:
                         token_stream+=token_item
+                    
                     token_map.append((token_stream, 'FLOAT'))
                     token_map.append((token, token_property))
+                    
                     is_decimal = False
-                    character_list.clear()
+                    evalutated_token_stream = ""
+                
                 else:
-                    token_map.append((token, token_property if token_property else 'UNKNOWN'))
+                    token_map.append((token, token_property))
+                    
                     is_integer: bool = False
                     is_decimal: bool = False
                     is_string_token: bool = False
-        if character_list:
+        if evalutated_token_stream:
             token_stream = ""
-            for token_item, _ in character_list:
+
+            for token_item in evalutated_token_stream:
                 token_stream+=token_item
+
             if is_decimal:
                 token_map.append((token_stream, 'FLOAT'))
+
             elif is_string_token:
                 token_map.append((token_stream, 'STRING'))
+
             elif is_integer:
                 token_map.append((token_stream, 'NUMBER'))
+
             else:
                 token_map.append((token, 'UNKNOWN'))
-            character_list.clear()
+
+            evalutated_token_stream = ""
+
         token_map[0] = (token_map[0][0], 'STREAM-IDENTIFIER')
         return token_map
