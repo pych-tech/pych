997845785cd2a87f83350a8838697ce2ac5d69d2
stomar117 <tomarshubhanshu@protonmail.ch>
2022-06-02 02:49:11 +0000

    Added some context for `Token` type

--- modules/utils/lexer.py
+++ modules/utils/lexer.py
@@ -3,6 +3,13 @@ from dataclasses import dataclass, field
 
 @dataclass(frozen=True)
 class Token:
+    """
+    Simple dataclass to represent a token with its type,
+    
+    Following are the attributes of `Token` type
+    - `token_value`: represents the value stored in the token
+    - `token_type`: represents the type of token stored
+    """
     token_value: str
     token_type: str = field(default_factory="UNKONWN")
 
