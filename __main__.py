b935c46d6a3722250cc15d6cb22509b4384178bf
Shubhanshu Tomar <tomarshubhanshu@protonmail.ch>
2022-06-18 15:44:29 +0530

    Use python naming scheme

---
+++ __main__.py
@@ -0,0 +1,19 @@
+from sys import exit
+from argparse import ArgumentParser
+from interpreter.interpreter import Interpreter
+
+parser = ArgumentParser()
+
+parser.add_argument("-i", "--interactive",
+    help="start the interactive session",
+    action="store_true"
+)
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    if args.interactive:
+        exit(Interpreter(kbd_count_break=True).start())
+    else:
+        print("non interactive sessions not yet implemented")
+        exit(1)
